3be7998818f8449e2243c0dc2e672d4ea005d93c
VuyoZumani <vuyolwethu.zumani@gmail.com>
2019-03-02 17:12:36 +0200

    FSharpKoans modification up to AboutIteration.fs

--- FSharpKoans/AboutIteration.fs
+++ FSharpKoans/AboutIteration.fs
@@ -16,7 +16,9 @@ module ``05: To iterate is human; to recurse, divine`` =
                 | true -> converge (d+10) c (n+1)
                 | false -> converge (d - 1) c (n+1)
             | true -> n
-        converge 3 10  |> should equal __
+        converge 3 10 1|> should equal 5
+        converge 3 10 2|> should equal 6
+        converge 3 10 100|> should equal 104
 
     [<Test>]
     let ``02 Tail recursion stops a stack overflow from occurring`` () =
@@ -30,4 +32,4 @@ module ``05: To iterate is human; to recurse, divine`` =
                 | false -> -1 + inner (count+1)
             inner sq
 
-        myfun 12 |> should equal
+        myfun 12 |> should equal -429981552
